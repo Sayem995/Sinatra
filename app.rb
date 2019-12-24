bca7ade5b8a404933aebabf927985a214070f34b
Sayem995 <sayem.bokhari.1@gmail.com>
2019-12-24 16:49:16 +0000

    introduced params to new named-cat route, renamed routes.

--- app.rb
+++ app.rb
@@ -8,8 +8,13 @@ get '/seret' do
   'hello Sayem, how are you?'
 end
 
-get '/cat' do
-  @name = ["Amigo", "Oscar", "Viking"].sample 
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
+end
 
+get '/named-cat' do
+  p params[:name]
+  @name = params[:name]
+  erb(:index)
 end
